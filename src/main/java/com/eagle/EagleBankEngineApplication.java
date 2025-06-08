d9a3f14c81f2f39d227e6a49d16d72ae4d16ac40
awatway <patwardhan.aditi@gmail.com>
2025-06-08 14:24:49 +0100

    Initial commit

---
+++ src/main/java/com/eagle/EagleBankEngineApplication.java
@@ -0,0 +1,13 @@
+package com.eagle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleBankEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleBankEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eagle/EagleBankEngineApplicationTests.java
@@ -0,0 +1,28 @@
+package com.eagle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class EagleBankEngineApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void actuatorHealthChecks() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+}
